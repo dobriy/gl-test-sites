3c9ea6b1586c5d21889444306229574a4c3c62f4
Alexey <dobriy1@gmail.com>
2024-10-07 18:07:05 +0100

    10 count and form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { instanceOf } from 'prop-types';
 import { withCookies, Cookies } from 'react-cookie';
 import './App.css';
 
-const COUNT_LIMIT = 100;
+const COUNT_LIMIT = 10;
 
 // can we use React hooks instead??
 // probably but for now we do it the old 
@@ -30,24 +30,79 @@ class App extends React.Component {
       }), function () {cookies.set('count', this.state.count, { path: '/' })});
    }
 
-   render(){
-       var hidden = this.state.count > COUNT_LIMIT ? 
-            <div>
-              <div>You are in!</div> 
-              <button onClick={() => {window.location.href="https://www.glastonburyfestivals.co.uk/";}}>Click me</button>
-              </div>: 
-            <p>
-              Number of hits: {this.state.count}
-            </p>;
+   render() {
+    let display = (
+        <p>
+            Number of hits: {this.state.count}
+        </p>
+    ); // Declare `display` outside the if condition to ensure it's accessible in the entire method scope.
+  
+    if (this.state.count > COUNT_LIMIT) {
+      display = this.regform();
 
-       return (
-        <div className="App">
-          <header className="App-header">
-	    {hidden}
-          </header>
+    } else {
+      display = (
+        <p>
+          Number of hits: {this.state.count}
+        </p>
+      );
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {display}
+        </header>
+      </div>
+    );
+  }
+
+  regform() {
+    return <div className="g-outer g-grey tight">
+      <div className="g-inner">
+        <div className="g-grid">
+          <div className="g-grid-col x12">
+            <section className="g-ui-box">
+              <h1 style={{ fontSize: '28px' }}>Registration</h1>
+              <p>
+                Please enter the registration number and postcode for each person (aged 13 or over) for whom you are
+                buying a ticket. You may enter up to 4 people's registration details, but can only purchase 1 ticket per registration.
+              </p>
+              <p>Please enter your Glastonbury registration details below.</p>
+              <form method="post" action="/gfl/addregistrations" id="mainRegForm" noValidate>
+                <input id="GlastonburyEventId" name="GlastonburyEventId" type="hidden" value="DF-1450001" />
+
+                <h3 className="reg-header" style={{ marginTop: '30px' }}>YOUR DETAILS</h3>
+                <div className="add-registration lead-reg-marg">
+                  <label style={{ marginTop: '15px' }}>Registration Number: </label>
+                  <input className="numeric valid" id="registrations_0__RegistrationId" maxLength="30" name="registrations[0].RegistrationId" type="text" value="1090813594" />
+                  <label style={{ marginTop: '15px' }}>Postcode: </label>
+                  <input className="postcode valid" id="registrations_0__PostCode" maxLength="30" name="registrations[0].PostCode" style={{ textTransform: 'uppercase' }} type="text" value="E2 0HN" />
+                </div>
+
+                <h3 className="reg-header">ADD UP TO 3 ADDITIONAL REGISTRATIONS</h3>
+                <div className="add-registration">
+                  {Array.from({ length: 3 }).map((_, index) => (
+                    <div key={index}>
+                      <span className="guest-count">#{index + 1}</span>
+                      <label style={{ marginTop: '15px' }}>Registration Number: </label>
+                      <input className="numeric" id={`registrations_${index + 1}__RegistrationId`} maxLength="30" name={`registrations[${index + 1}].RegistrationId`} type="text" />
+                      <label style={{ marginTop: '15px' }}>Postcode: </label>
+                      <input className="postcode" id={`registrations_${index + 1}__PostCode`} maxLength="30" name={`registrations[${index + 1}].PostCode`} style={{ textTransform: 'uppercase' }} type="text" />
+                      <div className="reg-divider">&nbsp;</div>
+                    </div>
+                  ))}
+                </div>
+
+                <button className="g-button primary small">Proceed</button>
+                <p className="clear-form g-small-print"><a href="javascript:void(0)">Clear registration form</a></p>
+              </form>
+            </section>
+          </div>
         </div>
-       );
-   }
+      </div>
+    </div>;
+  }
 }
 
 export default withCookies(App);
